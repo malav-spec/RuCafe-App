4c176d46c3ae124d4c4c069786ef2ace2a1c824a
Herik Patel <67105093+HerikPatel@users.noreply.github.com>
2021-04-19 00:15:59 +0530

    Add files via upload

--- app/src/main/java/com/example/rucafe/CurrentOrderActivity.java
+++ app/src/main/java/com/example/rucafe/CurrentOrderActivity.java
@@ -1,14 +1,54 @@
 package com.example.rucafe;
-
 import androidx.appcompat.app.AppCompatActivity;
-
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.ListView;
+import java.util.ArrayList;
+import static android.provider.AlarmClock.EXTRA_MESSAGE;
+import static com.example.rucafe.MainActivity.currentOrder;
 
 public class CurrentOrderActivity extends AppCompatActivity {
 
+    private ListView list;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_current_order);
+        list = findViewById(R.id.list);
+
+        ArrayList<String> tutorials = currentOrder.makeAL();
+        tutorials.add("h");
+        ArrayAdapter<String> List = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, tutorials);
+        list.setAdapter(List);
+        list.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                String selectedItem = (String) parent.getItemAtPosition(position);
+                remove(position);
+            }
+        });
     }
+
+    private void remove(int pos){
+        setContentView(R.layout.activity_current_order);
+        list = findViewById(R.id.list);
+        ArrayList<String> tutorials = currentOrder.makeAL();
+        tutorials.add("hp");
+        ArrayAdapter<String> List = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, tutorials);
+        list.setAdapter(List);
+        Intent intent = new Intent(this, CurrentOrderActivity.class);
+        Button editText = (Button) findViewById(R.id.butt);
+        String message = editText.getText().toString();
+        intent.putExtra(EXTRA_MESSAGE, message);
+        startActivity(intent);
+        finish();
+
+
+
+    }
+
 }
